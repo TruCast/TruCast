04ce49b4fdf251d2e41bb853f7b51bb6d9764029
Joey Jarosz <trucastgaming@gmail.com>
2017-10-20 14:08:43 -0500

    added remoteAddr workaround

--- ws.go
+++ ws.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"strings"
+	"bytes"
 	"log"
 	"net/http"
+	"net"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -48,9 +51,81 @@ func newToken(session *tokbox.Session, role tokbox.Role, userId string) (string,
 	return session.Token(role, "userId="+userId, tokbox.Days30)
 }
 
-/* func subToken(session *tokbox.Session, subRole tokbox.Role, userId string) (string, error) {
-	return session.Token(subRole, "userId="+userId, tokbox.Days30)
-}*/
+type ipRange struct {
+    start net.IP
+    end net.IP
+}
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+    // strcmp type byte comparison
+    if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+        return true
+    }
+    return false
+}
+
+var privateRanges = []ipRange{
+    ipRange{
+        start: net.ParseIP("10.0.0.0"),
+        end:   net.ParseIP("10.255.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("100.64.0.0"),
+        end:   net.ParseIP("100.127.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("172.16.0.0"),
+        end:   net.ParseIP("172.31.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.0.0.0"),
+        end:   net.ParseIP("192.0.0.255"),
+    },
+    ipRange{
+        start: net.ParseIP("192.168.0.0"),
+        end:   net.ParseIP("192.168.255.255"),
+    },
+    ipRange{
+        start: net.ParseIP("198.18.0.0"),
+        end:   net.ParseIP("198.19.255.255"),
+    },
+}
+
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+    // my use case is only concerned with ipv4 atm
+    if ipCheck := ipAddress.To4(); ipCheck != nil {
+        // iterate over all our ranges
+        for _, r := range privateRanges {
+            // check if this ip is in a private range
+            if inRange(r, ipAddress){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+func getIPAdress(r *http.Request) string {
+    for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+        addresses := strings.Split(r.Header.Get(h), ",")
+        // march from right to left until we get a public address
+        // that will be the address right before our proxy.
+        for i := len(addresses) -1 ; i >= 0; i-- {
+            ip := strings.TrimSpace(addresses[i])
+            // header can contain spaces too, strip those out.
+            realIP := net.ParseIP(ip)
+            if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+                // bad address, go to next
+                continue
+            }
+            return ip
+        }
+    }
+    return ""
+}
 
 func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -134,7 +209,9 @@ func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
 			leave()
 			currentRoomId = message.Payload.(string)
 			isHost = userId != "" && userId == currentRoomId
-			room, messages := s.State.Join(currentRoomId, userId, r.RemoteAddr)
+			getIPAdress := getIPAdress(r)
+			//TODO allow multiple clients to join same room via LAN/RemoteAddr
+			room, messages := s.State.Join(currentRoomId, userId, getIPAdress)
 			roomMessages = messages
 
 			// Create the room's TokBox session if it does not exist.
